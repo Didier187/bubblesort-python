f8f7cab17af42508957349a505e5a7d8e5acfcac
Didier <Kdondidier@gmail.com>
2022-01-18 09:39:34 +0000

    comments

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 def bubblesort(arr):
+  #travel through the array and avoid index error range (0, len(arr)-1)
   for i in range (0, len(arr)-1):
+    #for every number at index(idx) perfrom the comparison and swap the numbers
     for idx in range(0, len(arr)-1):
       if arr[idx] > arr[idx+1]:
         arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
   print(arr)
-
+#generate random array for testing
 def gen_rand_arr(leng):
   rand_arr =[]
   for number in range(0,leng):
@@ -13,4 +15,4 @@ def gen_rand_arr(leng):
     rand_arr.append(number)
   return rand_arr
 
-bubblesort(gen_rand_arr(10))
+bubblesort(gen_rand_arr(5))
